21578117be9b5bf82d4f40934e595cd25eb43398
Aritra Kar <aritrakar2002@gmail.com>
2020-12-27 16:33:23 +0530

    Add / remove capability added

--- public/app.js
+++ public/app.js
@@ -27,12 +27,14 @@ function renderCafe(doc){
 }
 
 //Getting data
-db.collection('cafes').get().then((snapshot) => {
+/*
+db.collection('cafes').where("city", "==", "Kolkata").orderBy("city").get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc)
         console.log(doc.data())
     });
 })
+*/
 
 //Saving data
 form.addEventListener("submit", (e) => {
@@ -43,4 +45,18 @@ form.addEventListener("submit", (e) => {
     })
     form.name.value = ''
     form.city.value = ''
+})
+
+//Real-time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges()
+    changes.forEach(change => {
+        if(change.type == "added"){
+            renderCafe(change.doc)
+        }
+        else if (change.type == "removed"){
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']')
+            cafeList.removeChild(li)
+        }
+    })
 })
